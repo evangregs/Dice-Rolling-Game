0324c85424001d4edba6cb7627d7c1f623ffbc36
gregeva <evang0911@gmail.com>
2023-01-18 18:11:44 -0700

    Add project files.

---
+++ Dice Rolling Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dice_Rolling_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numRolls = 0;
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.WriteLine("How many times would you like to throw the dice?");
+            numRolls = Int16.Parse(Console.ReadLine());
+        }
+    }
+}
